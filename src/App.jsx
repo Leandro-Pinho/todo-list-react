de09fda619101f2daacbdc0a1acd002167f8d371
Leandro-Pinho <leandroessencia@gmail.com>
2023-07-29 12:11:54 -0300

    estilizando o icons e ordenando a lista

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,8 @@ function App() {
       {toDo && toDo.length ? '' : 'No Tasks...'}
 
       {toDo && toDo
+        // ordenando a listagem do primeiro para o ultimo
+        .sort((a, b) => a.id > b.id ? 1 : -1)
         .map((task, index) => {
           return (
             <React.Fragment key={task.id}>
@@ -63,6 +65,11 @@ function App() {
                 <div className={task.status ? 'done' : ''}>
                   <span className='taskNumber'>{index + 1}</span>
                   <span className='taskText'>{task.title}</span>
+                </div>
+                <div className="iconWrap">
+                  <span><FontAwesomeIcon icon={faCircleCheck} /></span>
+                  <span><FontAwesomeIcon icon={faPen} /></span>
+                  <span><FontAwesomeIcon icon={faTrashCan} /></span>
                 </div>
               </div>
             </React.Fragment>
